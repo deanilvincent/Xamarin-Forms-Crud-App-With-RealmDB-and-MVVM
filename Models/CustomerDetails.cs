973aad1a421be8cdb09d7a8bbc1e1a269637a6ec
Mark Deanil Vicente <markdeanilvicente@gmail.com>
2017-06-10 18:06:04 +0800

    Added Models folder

---
+++ Models/CustomerDetails.cs
@@ -0,0 +1,19 @@
+﻿using Realms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudWithRealmApp.Models
+{
+    public class CustomerDetails : RealmObject
+    {
+        [PrimaryKey]
+        public int CustomerId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public int CustomerAge { get; set; }
+    }
+}
