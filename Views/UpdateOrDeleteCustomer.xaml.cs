a7629080b2a28f09252ccb4e21b514a895e0f313
Mark Deanil Vicente <markdeanilvicente@gmail.com>
2017-06-10 18:15:53 +0800

    Added the xaml for updateordelete

---
+++ Views/UpdateOrDeleteCustomer.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CrudWithRealmApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UpdateOrDeleteCustomer : ContentPage
+    {
+        public UpdateOrDeleteCustomer()
+        {
+            InitializeComponent();
+        }
+    }
+}
